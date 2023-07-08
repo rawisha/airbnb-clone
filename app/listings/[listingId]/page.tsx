7b8b3fffb614eed0a39326a8dfb68c1ea72a1a97
rawisha <rawvsmaw22@gmail.com>
2023-07-08 09:12:43 +0200

    reservations finished

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,13 +3,15 @@ import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
-interface Iparams {
+interface IParams {
   listingId?: string;
 }
 
-const ListingPage = async ({ params }: { params: Iparams }) => {
+const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -21,7 +23,11 @@ const ListingPage = async ({ params }: { params: Iparams }) => {
   }
   return (
     <ClientOnly>
-      <ListingClient listing={listing} currentUser={currentUser} />
+      <ListingClient
+        listing={listing}
+        currentUser={currentUser}
+        reservations={reservations}
+      />
     </ClientOnly>
   );
 };
